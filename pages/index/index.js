f8724cc1d9fc4e210b5cc7e7e932b58c27250359
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-30 14:57:30 +0800

    菜单区域渲染完成

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,35 +5,30 @@ const app = getApp()
 
 Page({
   data: {
-    background: [
-      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
-      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
-      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
-    ],
-
-    menus: [
-      "../../images/cart_empty@2x.png",
-      "../../images/cart_empty@2x.png",
-      "../../images/cart_empty@2x.png",
-      "../../images/cart_empty@2x.png",
-    ]
+    background: [],
+    menus: []
   },
 
   // --------------
   onLoad() {
-
     request({
       url: "/api/public/v1/home/swiperdata"
-    }).then(res=>{
+    }).then(res => {
       const { message } = res.data;
       // 赋值给data
       this.setData({
         background: message
       })
-    })
+    }),
 
-  
-
+      request({
+        url: "/api/public/v1/home/catitems"
+      }).then(res => {
+        const { message } = res.data;
+        this.setData({
+          menus: message
+        })
+      })
   }
 
 })
