beae32e5f695345c5df128c9d1abfca12688d4d2
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-30 14:45:34 +0800

    首页布局以及轮播图

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,6 +4,36 @@ import request from "../../utils/request.js"
 const app = getApp()
 
 Page({
+  data: {
+    background: [
+      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
+      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
+      "http://img3.imgtn.bdimg.com/it/u=2849918286,1173942087&fm=26&gp=0.jpg",
+    ],
+
+    menus: [
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+      "../../images/cart_empty@2x.png",
+    ]
+  },
+
+  // --------------
+  onLoad() {
+
+    request({
+      url: "/api/public/v1/home/swiperdata"
+    }).then(res=>{
+      const { message } = res.data;
+      // 赋值给data
+      this.setData({
+        background: message
+      })
+    })
+
   
+
+  }
+
 })
- 
