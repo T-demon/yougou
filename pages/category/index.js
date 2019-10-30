432840edc0b6ce4b23c2359d381291467264f44a
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-30 19:53:50 +0800

    分类商品渲染完成

--- pages/category/index.js
+++ pages/category/index.js
@@ -1,4 +1,5 @@
 // pages/category/index.js
+import request from  "../../utils/request.js"
 Page({
 
   /**
@@ -12,8 +13,16 @@ Page({
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad(){
+      request({
+        url:"https://api.zbztb.cn/api/public/v1/categories",
+      }).then(res=>{
+        console.log(res.data)
+        const {message} = res.data
+        this.setData({
+          list: message
+        })
+      })
   },
 
   handleChange(event){
