c55e7c849a20454c2d224cf2cabe4614f6b84980
T-demon <52730601+T-demon@users.noreply.github.com>
2019-10-31 10:47:26 +0800

    跳转商品列表页获取参数

--- pages/goods_list/index.js
+++ pages/goods_list/index.js
@@ -14,6 +14,7 @@ Page({
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    console.log(options)
     request({
       url:"https://api.zbztb.cn/api/public/v1/goods/search"
     }).then(res=>{
