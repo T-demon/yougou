68ebcaa2fbf55e0657afc30b5c7b62ad47b480b3
T-demon <52730601+T-demon@users.noreply.github.com>
2019-11-01 15:50:20 +0800

    点击取消按钮清空输入框的值

--- pages/search/index.js
+++ pages/search/index.js
@@ -5,62 +5,32 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    isshow:false,
+    search:""
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    
 
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
+  handleInput(event){
+    const {value} = event.detail
+    this.setData({
+      isshow:true,
+      search:value.trim()
+    })
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  handleClear(){
+    this.setData({
+      isshow:false,
+      search:""
+    })
   }
+
+
 })
